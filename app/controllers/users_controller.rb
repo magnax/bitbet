eb29fdbf4c9b5959b7b8fb706bbe78be6b6b6fb6
Maciej N <magnax@gmail.com>
2014-04-01 14:45:41 +0200

    Formatting code (indentation)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,39 @@
 class UsersController < ApplicationController
-before_action :signed_in_user, only: [ :show ]
+  before_action :signed_in_user, only: [ :show ]
 
-def new
-	@user = User.new
-end
+  def new
+    @user = User.new
+  end
 
-def create
-	@user = User.new(user_params)
-	if @user.save
-		sign_in @user
-		flash[:success] = I18n.t 'flash.success.user_created'
-		begin
-			unless bitcoin_client.create_user_account(@user)
-				flash[:notice] = I18n.t 'flash.notice.user_account_failed'
-			end
-		rescue BitcoinClient::ConnectionError
-			flash[:error] = I18n.t 'flash.error.client_error'
-		end
-		redirect_to root_path
-	else
-		render 'new'
-	end
-end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = I18n.t 'flash.success.user_created'
+      begin
+        unless bitcoin_client.create_user_account(@user)
+          flash[:notice] = I18n.t 'flash.notice.user_account_failed'
+        end
+      rescue BitcoinClient::ConnectionError
+        flash[:error] = I18n.t 'flash.error.client_error'
+      end
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 
-def show
-	@user = current_user
-end
+  def show
+    @user = current_user
+  end
 
-def name_availability
-	render :text => ((User.find_by_name(params[:name])) ? "this name is not available" : "OK!")
-end
+  def name_availability
+    render :text => ((User.find_by_name(params[:name])) ? "this name is not available" : "OK!")
+  end
 
 private
 
-def user_params
-	params.require(:user).permit(:name, :email, :password, :password_confirmation, :ref_id) 
-end
-
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :ref_id)
+  end
 end
