cd4b279cd8c4070ae4aa641366f0e0df3eb9484b
Maciej N <magnax@gmail.com>
2014-04-01 22:50:23 +0200

    Formatting code in helpers (indentation)

--- app/helpers/bets_helper.rb
+++ app/helpers/bets_helper.rb
@@ -1,11 +1,10 @@
 module BetsHelper
 
-def view_as_btc(amount, precision)
-	number_with_precision(stc_to_btc(amount), precision: precision, separator: ',', delimiter: '.')
-end
+  def view_as_btc(amount, precision)
+    number_with_precision(stc_to_btc(amount), precision: precision, separator: ',', delimiter: '.')
+  end
 
-def stc_to_btc(stc_amount)
-	stc_amount / 100000000.0
-end
-
+  def stc_to_btc(stc_amount)
+    stc_amount / 100000000.0
+  end
 end
